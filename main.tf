0c299147f0e07939549907f1f60bef77efe50844
Prajwal <prajwal3.intern@phonepe.com>
2024-05-10 22:20:14 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    multipass = {
+      source  = "larstobi/multipass"
+      version = "1.4.2"
+    }
+  }
+}
+
+provider "multipass" {
+  alias = "nginx_logging_analysis"
+}
+
+resource "multipass_instance" "node" {
+  count          = var.no_of_nodes
+  name           = "node-${count.index + 1}"
+  cloudinit_file = "${path.module}/user_data.cfg"
+  cpus           = var.node_cpu
+  image          = var.release
+  memory         = "${var.node_memory}MiB"
+}
---
+++ var.tf
@@ -0,0 +1,23 @@
+variable "no_of_nodes" {
+  type        = number
+  default     = 2
+  description = "Total Number of Instances to be provisioned"
+}
+
+variable "release" {
+  type        = string
+  default     = "jammy"
+  description = "Release of the Ubuntu"
+}
+
+variable "node_cpu" {
+  type        = number
+  default     = 1
+  description = "CPU for Node"
+}
+
+variable "node_memory" {
+  type        = number
+  default     = 1024
+  description = "RAM for Node"
+}
