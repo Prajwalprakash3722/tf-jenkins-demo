e36bbb4b78feacafb31d6c7e3d5e9919d48e161b
Prajwal prakash <prajwalprakash3722@gmail.com>
2024-05-10 22:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "multipass" {
 
 resource "multipass_instance" "node" {
   count          = var.no_of_nodes
-  name           = "node-${count.index + 1}"
+  name           = "node-${count.index + 5}"
   cloudinit_file = "${path.module}/user_data.cfg"
   cpus           = var.node_cpu
   image          = var.release
